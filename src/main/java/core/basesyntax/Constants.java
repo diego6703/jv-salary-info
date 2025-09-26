032019098027ac7259b7b272e28cf8531497d5f9
Jakub Cybulski <jakub_cybulski@o2.pl>
2025-09-26 11:24:57 +0200

    Salary Info Task full implementation

---
+++ src/main/java/core/basesyntax/Constants.java
@@ -0,0 +1,9 @@
+package core.basesyntax;
+
+public class Constants {
+    public static final String PARSE_FORMAT = "dd.MM.yyyy";
+    public static final String WRONG_DATE_FORMAT_MESSAGE = "wrong date format";
+    public static final String REPORT_BEGINNING = "Report for period ";
+    public static final String DATA_SPLIT_SEPARATOR = " ";
+    public static final String DATA_LIST_SEPARATOR = " - ";
+}
--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,61 @@
 package core.basesyntax;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 public class SalaryInfo {
+    private final SimpleDateFormat dateFormat = new SimpleDateFormat(Constants.PARSE_FORMAT);
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        int numberOfEmployees = names.length;
+        int[] employeesEarnings = new int[numberOfEmployees];
+        String[] singleEmployeeData;
+        String singleEmployeeName;
+        long singleEmployeeWorkingDayDate;
+        long startDate;
+        long endDate;
+        int singleEmployeeHoursWorked;
+        int singleEmployeeEarningsPerHour;
+        int singleEmployeeEarnings;
+        try {
+            startDate = dateFormat.parse(dateFrom).getTime();
+            endDate = dateFormat.parse(dateTo).getTime();
+        } catch (ParseException e) {
+            throw new RuntimeException(Constants.WRONG_DATE_FORMAT_MESSAGE);
+        }
+        for (String employeeData : data) {
+            singleEmployeeData = employeeData.split(Constants.DATA_SPLIT_SEPARATOR);
+            try {
+                singleEmployeeWorkingDayDate = dateFormat.parse(singleEmployeeData[0]).getTime();
+            } catch (ParseException e) {
+                throw new RuntimeException(Constants.WRONG_DATE_FORMAT_MESSAGE);
+            }
+            if (singleEmployeeWorkingDayDate >= startDate && singleEmployeeWorkingDayDate <= endDate) {
+                singleEmployeeName = singleEmployeeData[1];
+                singleEmployeeHoursWorked = Integer.parseInt(singleEmployeeData[2]);
+                singleEmployeeEarningsPerHour = Integer.parseInt(singleEmployeeData[3]);
+                singleEmployeeEarnings = singleEmployeeHoursWorked * singleEmployeeEarningsPerHour;
+                for (int i = 0; i < numberOfEmployees; i++) {
+                    if (singleEmployeeName.equals(names[i])) {
+                        employeesEarnings[i] += singleEmployeeEarnings;
+                    }
+                }
+            }
+        }
+        sb.append(Constants.REPORT_BEGINNING)
+                .append(dateFrom)
+                .append(Constants.DATA_LIST_SEPARATOR).append(dateTo);
+
+        for (int i = 0; i < numberOfEmployees; i++) {
+            sb.append(System.lineSeparator())
+                    .append(names[i])
+                    .append(Constants.DATA_LIST_SEPARATOR)
+                    .append(employeesEarnings[i]);
+        }
+
+        return sb.toString();
     }
+
+
 }
